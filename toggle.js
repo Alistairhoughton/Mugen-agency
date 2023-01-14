50164faea8ab92a8d547ad4236675cbd7f64de91
Alistair Houghton <alistair.g.houghton@gmail.com>
2023-01-14 23:03:45 +0000

    updated darkmode

--- toggle.js
+++ toggle.js
@@ -1,24 +1,30 @@
 const toggle = document.getElementById("toggle");
 const sections = document.getElementsByClassName("section");
+const lights = document.getElementsByClassName("light");
+const lightsSvg = document.getElementsByClassName("light-svg");
 
 toggleHandler = () => {
   if (toggle.checked === false) {
-    console.log("false");
     sections[0].setAttribute("data-bg", "#23b296");
     sections[1].setAttribute("data-bg", "#f15f61");
     sections[2].setAttribute("data-bg", "#DDBCF7");
     sections[3].setAttribute("data-bg", "#fca971");
   }
   if (toggle.checked === true) {
-    console.log("true");
-    sections[0].setAttribute("data-bg", "grey");
-    sections[1].setAttribute("data-bg", "grey");
-    sections[2].setAttribute("data-bg", "grey");
-    sections[3].setAttribute("data-bg", "grey");
+    sections[0].setAttribute("data-bg", "#1A1C52");
+    sections[1].setAttribute("data-bg", "#1A1C52");
+    sections[2].setAttribute("data-bg", "#1A1C52");
+    sections[3].setAttribute("data-bg", "#1A1C52");
   }
-  console.log([...sections]);
-  console.log(sections[0].dataset.bg);
+
   onScroll();
+  [...lights].forEach((el) => {
+    el.classList.toggle("dark");
+  });
+
+  [...lightsSvg].forEach((el) => {
+    el.classList.toggle("dark-svg");
+  });
 };
 
 toggle.addEventListener("click", toggleHandler);
